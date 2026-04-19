43d2ad2b2f74b5dba0ef628a2e2fbf75463a52e2
Moises M <m.moises.p@gmail.com>
2026-04-19 11:44:50 -0500

    Update Rubro.js

--- backend/src/models/Rubro.js
+++ backend/src/models/Rubro.js
@@ -9,7 +9,6 @@ const sequelize = require('../config/database');
 const Rubro = sequelize.define('Rubro', {
 
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-
   nombre: { type: DataTypes.STRING(80), allowNull: false },
   codigo: { type: DataTypes.STRING(30), allowNull: false, unique: true },
   descripcion: { type: DataTypes.STRING(300), allowNull: true },
@@ -18,19 +17,5 @@ const Rubro = sequelize.define('Rubro', {
   tableName: 'rubros',
   timestamps: false
 });
-
-  nombre: { type: DataTypes.STRING(100), allowNull: false, unique: true },
-  descripcion: { type: DataTypes.STRING(300), allowNull: true }
-}, { tableName: 'rubros', timestamps: false });
-
-
-  id:          { type: DataTypes.INTEGER,     primaryKey: true, autoIncrement: true },
-  nombre:      { type: DataTypes.STRING(100), allowNull: false, unique: true },
-  descripcion: { type: DataTypes.STRING(300), allowNull: true }
-}, {
-  tableName:  'rubros',
-  timestamps: false
-});
-
 
 module.exports = Rubro;
